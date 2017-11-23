844dc00f5fbbde2b72c0cf542d053a6e5fe1f3a0
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2017-11-23 17:53:52 +0100

    fixed install.sh

--- install.sh
+++ install.sh
@@ -1,36 +1,36 @@
 #!/bin/sh
 APP_HOME="arpsentinel-applet"
 APP_UUID="arpsentinel@arpsentinel.github.io"
-DBUS_SERVICE=$(
-cat<<EOF
-[D-BUS Service]\n
-Name=org.arpsentinel\n
-Exec="$HOME/.local/share/cinnamon/applets/arpsentinel@arpsentinel-applet.github.io/bin/arpalert-service.py"\n
-User=arpalert
-EOF
-)
+CINNAMON_APPLETS="$HOME/.local/share/cinnamon/applets/"
+DBUS_SERVICE="[D-BUS Service]\nName=org.arpsentinel\nExec=$HOME/.local/share/cinnamon/applets/$APP_UUID/bin/arpalert-service.py\nUser=arpalert"
 
-echo -n "[+] installing extension: "
-out=$(sudo /bin/cp -a $APP_UID/ "~/./local/share/cinnamon/applets/")
-[ "$out" = "0" ] && echo " OK" || echo " KO";exit
+error(){
+  echo " KO"
+  exit
+}
+
+mkdir -p $CINNAMON_APPLETS 2>/dev/null
+out=$(/bin/cp -a $APP_UUID/ $CINNAMON_APPLETS)
+[ "$?" = "0" ] && echo " OK" || error
 
 echo -n "[+] Enable ARPSentinel system service: "
-out=$(sudo /bin/echo $DBUS_SERVICE > /usr/share/dbus-1/system-services/org.arpsentinel.service)
-[ "$out" = "0" ] && echo " OK" || echo " KO";exit
+echo -e $DBUS_SERVICE
+out=$(/bin/echo -e $DBUS_SERVICE > arpsentinel.service.temp; sudo /bin/cp arpsentinel.service.temp /usr/share/dbus-1/system-services/org.arpsentinel.service)
+[ "$?" = "0" ] && echo " OK" || error
 out=$(sudo /bin/cp ./arpsentinel.conf /etc/dbus-1/system.d/)
-[ "$out" = "0" ] && echo " OK" || echo " KO";exit
+[ "$?" = "0" ] && echo " OK" || error
 
 echo "[+] Now modify the following options of /etc/arpalert/arpalert.conf:"
 echo "    maclist file = "$HOME/$APP_HOME/maclist.allow""
 echo "    maclist alert file = "$HOME/$APP_HOME/maclist.deny""
 echo "    maclist leases file = "$HOME/$APP_HOME/arpalert.leases""
 
-sleep 1
+sleep 2
 # check if /etc/sudoers.d/ exists
 # if [ -d '/etc/sudoers.d' ] ...
-echo "[+] Add this line to /etc/sudoers to effectively block offenders:"
+echo "[+] And add this line to /etc/sudoers to effectively block offenders:"
 echo "    user host = (root) NOPASSWD: $HOME/$APP_HOME/block_mac.sh"
 
-sleep 1
+sleep 2
 
 echo "DONE"
