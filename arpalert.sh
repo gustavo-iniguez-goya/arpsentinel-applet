97d558cf095d5180f87ad25590920156900c5118
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2017-11-25 10:54:39 +0100

    updated gdbus example

--- arpalert.sh
+++ arpalert.sh
@@ -7,9 +7,9 @@ hostname=$(hostname)
 #logger $*
 
 # Other methods
-#gdbus call --session --dest org.gnome.Shell --object-path /com/michalrus/TextInTaskBar --method com.michalrus.TextInTaskBar.setText "$1" "$2" "$3" "$4" "$5" "$6"
 #dbus-send --session --type=method_call --dest='org.gnome.Shell' '/com/michalrus/TextInTaskBar' com.michalrus.TextInTaskBar.setText string:"$1" string:"$2" string:"$3" string:"$4" string:"$5" string:"$6"
 #dbus-send --system --type=method_call --dest='org.gnome.Shell' '/com/michalrus/TextInTaskBar' com.michalrus.TextInTaskBar.setText string:"$1" string:"$2" string:"$3" string:"$4" string:"$5" string:"$6"
 # dbus-send --session --type=method_call --dest='org.gnome.Shell' '/org/arpsentinel' org.arpsentinel.Message.setText string:"1" string:"r" string:"n" string:"b" string:"v" string:"a"
+#gdbus call --system --dest org.arpsentinel --object-path /org/arpsentinel --method org.arpsentinel.alerts.sendAlert "$1" "$2" "$3" "$4" "$5" "$6"
 
 qdbus --system org.arpsentinel /org/arpsentinel sendAlert "$1" "$2" "$3" "$4" "$5" "$6"
