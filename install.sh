84e9a2f93be4ee78ae5dc648ced5e2de27c337f9
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2018-01-04 23:09:51 +0100

    added gpl text

--- install.sh
+++ install.sh
@@ -1,4 +1,19 @@
 #!/bin/sh
+# This file is part of ARP Sentinel.
+#
+#    ARP Sentinel is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 APP_HOME="arpsentinel-applet"
 APP_UUID="arpsentinel@$APP_HOME.github.io"
 CINNAMON_APPLETS="$HOME/.local/share/cinnamon/applets/"
