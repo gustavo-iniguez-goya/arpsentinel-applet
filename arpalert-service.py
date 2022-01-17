67482fc63dfd76196544e70db3134e98eda97c88
Gustavo Iñiguez Goia <gooffy1@gmail.com>
2022-01-17 01:28:20 +0100

    dbus service updated to python3

--- arpalert-service.py
+++ arpalert-service.py
@@ -1,20 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import dbus
 import dbus.service
 from gi.repository import GObject, GLib
 from dbus.mainloop.glib import DBusGMainLoop
 
 # qdbus --system org.arpsentinel /org/arpsentinel sendAlert orig_mac, orig_ip, x, device, alert_type, mac_vendor
- 
+
 class ArpSentinel(dbus.service.Object):
     def __init__(self):
         self.session_bus = dbus.SystemBus()
         name = dbus.service.BusName("org.arpsentinel", bus=self.session_bus)
         dbus.service.Object.__init__(self, name, '/org/arpsentinel')
-   
+
     @dbus.service.signal('org.arpsentinel.alerts')
     def getAlert(self, orig_mac, orig_ip, x, device, alert_type, mac_vendor=''):
-        print "%s,%s,%s,%s,%s,%s" % (orig_mac, orig_ip, x, device, alert_type, mac_vendor)
+        print("%s,%s,%s,%s,%s,%s" % (orig_mac, orig_ip, x, device, alert_type, mac_vendor))
         return orig_mac
 
     @dbus.service.method("org.arpsentinel.alerts", in_signature='ssssss', out_signature='as')
